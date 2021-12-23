c99045962c42af6ff474a90907fd13d5f5203456
shädam <47268949+supahero1@users.noreply.github.com>
2021-12-23 10:11:45 +0100

    Create api.js

---
+++ api.js
@@ -0,0 +1,134 @@
+function diep_api() {
+  "use strict";
+  return Symbol.for("diep.api");
+}
+(async function() {
+  "use strict";
+  function log(...a) {
+    console.log("[diep.api] " + a.join(""));
+  }
+  function err(...a) {
+    console.log("[diep.api.err] " + a.join(""));
+  }
+  const win = typeof unsafeWindow == "undefined" ? window : unsafeWindow;
+  if(win.input || document.getElementById("canvas") != undefined) {
+    return err("a script tried launching diep.api without \"@run-at\" set to \"document-body\"");
+  }
+  const api = diep_api();
+  if(win[api]) {
+    return;
+  }
+  
+  log("init");
+  Reflect.defineProperty(win, api, {
+    value: new class {
+      #events;
+      constructor() {
+        this.#events = new Map;
+        
+        this.player = {
+          x: NaN,
+          y: NaN,
+          mouse: {
+            x: NaN,
+            y: NaN
+          }
+        };
+      }
+      on(what, cb) {
+        let set = this.#events.get(what);
+        if(!set) {
+          set = new Set([cb]);
+          this.#events.set(what, set);
+          return;
+        }
+        set.add(cb);
+      }
+      emit(what, ...args) {
+        const set = this.#events.get(what);
+        if(!set) {
+          return;
+        }
+        for(const cb of set) {
+          cb(...args);
+        }
+      }
+      remove(what, cb) {
+        if(typeof cb == "undefined") {
+          this.#events.delete(what);
+          return;
+        }
+        this.#events.get(what)?.delete(cb);
+      }
+    },
+    configurable: false,
+    enumerable: false
+  });
+  Reflect.preventExtensions(win[api]);
+  
+  Reflect.defineProperty(win, "input", {
+    set: function(to) {
+      delete win.input;
+      win.input = to;
+      win[api].emit("ready");
+      log("ready");
+    },
+    get: function() {
+      return undefined;
+    },
+    configurable: true
+  });
+  
+  await new Promise(function(resolve) {
+    new MutationObserver(function(list, observer) {
+      list.forEach(function(mut) {
+        if(mut.addedNodes[0].id == "canvas") {
+          win[api].emit("pre.ready");
+          observer.disconnect();
+          resolve();
+        }
+      });
+    }).observe(document.body, { childList: true });
+  });
+  
+  const c = win.CanvasRenderingContext2D;
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  
+  let pixel_scale = win.devicePixelRatio;
+  canvas.width = win.innerWidth * pixel_scale;
+  canvas.height = win.innerHeight * pixel_scale;
+  let ui_scale = 1;
+  
+  function get_ratio() {
+    if(canvas.height * 16 / 9 >= canvas.width) {
+      return canvas.height * ui_scale;
+    } else {
+      return canvas.width / 16 * 9 * ui_scale;
+    }
+  }
+
+  function get_scale() {
+    if(canvas.height * 16 / 9 >= canvas.width) {
+      return canvas.height / 1080;
+    } else {
+      return canvas.width / 1920;
+    }
+  }
+  
+  let ratio = get_ratio();
+  let scale = get_scale();
+  
+  function dynamic_update() {
+    pixel_scale = win.devicePixelRatio;
+    if(canvas.width != win.innerWidth * pixel_scale || canvas.height != win.innerHeight * pixel_scale) {
+      canvas.width = win.innerWidth * pixel_scale;
+      canvas.height = win.innerHeight * pixel_scale;
+    }
+    ratio = get_ratio();
+    scale = get_scale();
+    
+    requestAnimationFrame(dynamic_update);
+  }
+  requestAnimationFrame(dynamic_update);
+})();
